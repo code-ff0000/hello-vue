5215bdf495868d6fee7fadbf4f791ac0bb720567
Ryan Sargent <code_red@space-command.local>
2023-06-23 23:08:24 -0500

    Install @nuxthq/ui module, and remove welcome

---
+++ components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <UContainer class="h-16 w-full bg-primary flex px-5 border-b border-green-500">
+      <p class="font-mono my-auto">Hello Vue</p>
+    </UContainer>
+  </header>
+</template>
